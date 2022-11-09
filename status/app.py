d0a399b9911303812043bcb1042f3f7ada2ee4e4
Alec Snyder <alec@asnyder.net>
2022-11-09 16:10:53 +0200

    add status component

---
+++ status/app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from pymongo import MongoClient
+import uuid
+
+app = Flask(__name__)
+
+connection_client = MongoClient("mongodb://mongo:password@localhost", 27017)
+mongo_client = connection_client.at_bay_db.scans
+
+def get_mongo_client():
+  client = MongoClient("localhost", 27017)
+  return client.at_bay_db
+
+def insert_scan_request(uid):
+  scan_obj = { "_id": uid, "status": "Accepted" }
+  val = mongo_client.insert_one(scan_obj)
+
+def format_one(v):
+  return "%s : %s" % (v['_id'], v['status'])
+
+def format_all(vs):
+  result = ""
+  for v in vs:
+    result += format_one(v) + "<br>\r\n"
+  return result
+
+@app.route("/", methods=['GET'])
+def index():
+  results = mongo_client.find()
+  return format_all(results)
+
+@app.route("/<scan_id>", methods=['GET'])
+def single_scan(scan_id):
+    result = mongo_client.find_one({"_id": scan_id})
+    if result is None:
+      return "%s : Not Found <br>\r\n" % scan_id, 404
+    else:
+      return format_one(result)
